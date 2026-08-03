6faf387fe7446ed151ff20c9a714e3186ee86e8a
89fvtpm8th-hue <89fvtpm8th@privaterelay.appleid.com>
2026-08-02 21:53:23 -0300

    Vava3.0

--- js/script.js
+++ js/script.js
@@ -1,24 +1,25 @@
-document.getElementById("login").onclick = function () {
-    window.location.href = "Tela.html";
-};
-
-
 
 //Dados de ussuarios 
-const user2 = "Gustavo";
-const password2 = 1234;
+const user2 = "Nubia";
+const password2 = "1234";
 
 //Inputs,button
 const InputUsername = document.getElementById('user');    
 const InputPassword = document.getElementById('password');
-const button = document.getElementById('login')
+const button = document.getElementById('login');
 
-button.addEventListener("click", verificacao)  
+button.addEventListener("click", verificacao);
 function verificacao(){
     const UserDigitado = InputUsername.value.trim();
     const SenhaDigitado = InputPassword.value.trim();
+    if(UserDigitado  == user2 && SenhaDigitado == password2 ){window.location.href="Tela.html"}
+    else{(alert("Error"))};
+    
 };
 
 
 
+
+
+   
 
