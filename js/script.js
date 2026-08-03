ec95f353f5abecc0b68b72575b02847eae812c95
89fvtpm8th-hue <89fvtpm8th@privaterelay.appleid.com>
2026-08-02 21:55:51 -0300

    vavava

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ function verificacao(){
     const UserDigitado = InputUsername.value.trim();
     const SenhaDigitado = InputPassword.value.trim();
     if(UserDigitado  == user2 && SenhaDigitado == password2 ){window.location.href="Tela.html"}
-    else{(alert("Error"))};
+    else{(alert("ME manda um pix de 200"))};
     
 };
 
